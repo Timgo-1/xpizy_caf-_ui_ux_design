43223a26faf112e3de6ed40463162ab295e72584
Timgo-1 <timgodesigns@gmail.com>
2022-04-15 01:59:10 +0300

    xpicy-cafe

---
+++ README.md
@@ -0,0 +1,18 @@
+# Restaurant Landing Page
+### [Live Site](https://xpizy-cafe.ga/)
+
+![Restaurant Landing Page](https://i.ibb.co/rtDtFwq/xpizy-cafe.png)
+
+## Stay up to date with new projects
+New major projects coming soon.
+
+## Introduction
+This is a code repository for Xpizy-Cafe Restaurant Landing Page. It entails building a Modern UI/UX Restaurant Landing Page Website utilising the the underlisted processes:
+
+- React file and folder structure;
+- React Functional components and their reusability;
+- Fundamentals of the CSS BEM Model;
+- Fundamental CSS properties to master flex & grid;
+- From soft and pleasant animations to complex gradients;
+- Perfectly placed media queries for satisfactory responsiveness covering almost devices;
+- And finally the website is deployed to 'high-speed' servers, alongside giving it a custom domain name.
---
+++ src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Irish Guinness',
+    price: '$26',
+    tags: 'IE | 750 ml',
+  },
+  {
+    title: 'Rhino Pale Ale',
+    price: '$31',
+    tags: 'CA | 750 ml',
+  },
+  {
+    title: 'Catena Malbee',
+    price: '$59',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'La Vieillw Rose',
+    price: '$44',
+    tags: 'FR | 750 ml',
+  },
+  {
+    title: 'Chapel Hill Shiraz',
+    price: '$56',
+    tags: 'AU | Bottle',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Negroni',
+    price: '$26',
+    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+  },
+  {
+    title: 'Old Fashioned',
+    price: '$31',
+    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+  },
+  {
+    title: "Dark 'N' Stormy",
+    price: '$16',
+    tags: 'Dark rum | Ginger beer | Slice of lime',
+  },
+  {
+    title: 'Daiquiri',
+    price: '$10',
+    tags: 'Rum | Citrus juice | Sugar',
+  },
+  {
+    title: 'Aperol Sprtiz',
+    price: '$20',
+    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award02,
+    title: 'Best Dessert',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { wines, cocktails, awards };
